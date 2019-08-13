142d0e01476674d836a4e4c2b8285b67bbe27ced
G.N.Pang <gnpang@users.noreply.github.com>
2019-08-13 14:58:22 -0600

    Add files via upload

--- gpar/postProcess.py
+++ gpar/postProcess.py
@@ -713,7 +713,7 @@ class glanceEQ(QtWidgets.QMainWindow):
 				ax.set_title(name)
 			if self._current_type == 'slowness':
 				a = u"\u00b0"
-				title = 'Earthquake: $s\nSlant Stack at a Backazimuth of %.1f %sN'%(self._current_event.ID,self._current_event.bakAzimuth,a)
+				title = 'Earthquake: %s\nSlant Stack at a Backazimuth of %.1f %sN'%(self._current_event.ID,self._current_event.bakAzimuth,a)
 			elif self._current_type == 'theta':
 				title = 'Earthquake: %s\nSlant Stack at a slowness of %.2f s/deg'%(self._current_event.ID,self._current_event.rayParameter)
 			self.fig.suptitle(title)
